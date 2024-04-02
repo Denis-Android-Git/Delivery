905542a85a10c7b848de6e30179f73927b8e2819
Denis-Android-Git <124072945+Denis-Android-Git@users.noreply.github.com>
2024-04-02 13:08:57 +0300

    n

--- app/src/main/java/com/example/delivery/screens/DbItem.kt
+++ app/src/main/java/com/example/delivery/screens/DbItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -92,17 +93,19 @@ fun DbItem(
             horizontalAlignment = Alignment.End
         ) {
             Text(
-                text = "460 p",
+                text = stringResource(R.string.pricedb, productFromDb.price),
                 fontSize = 16.sp,
                 fontFamily = FontFamily(Font(R.font.roboto)),
                 fontWeight = FontWeight.Bold
             )
-            Text(
-                text = "500 p",
-                fontSize = 14.sp,
-                fontFamily = FontFamily(Font(R.font.roboto)),
-                textDecoration = TextDecoration.LineThrough,
-            )
+            if (productFromDb.oldPrice != null) {
+                Text(
+                    text = stringResource(R.string.pricedb, productFromDb.oldPrice!!),
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.roboto)),
+                    textDecoration = TextDecoration.LineThrough,
+                )
+            }
         }
         HorizontalDivider(
             modifier = Modifier.align(Alignment.BottomCenter),
